342d4df8dce99c3482a02fb11f938e2abd2888b9
sammotaroki <sammotaroki@gmail.com>
2021-03-14 11:11:10 +0300

    initial commit

---
+++ frontend/src/data.js
@@ -0,0 +1,94 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Organic Bathbomb',
+            category: 'Soap',
+            image: '/images/bathbomb.jpg',
+            price: 9.99,
+            brand: 'Mikalla',
+            rating: 4,
+            numReviews: 13,
+            description: 'good quality'
+        },
+        {
+            _id: '2',
+            name: 'Bubble bath',
+            category: 'Soap',
+            image: '/images/bubblebath.jpeg',
+            price: 18.99,
+            brand: 'Mikalla',
+            rating: 4,
+            numReviews: 13,
+            description: 'good quality shampoo'
+        },
+        {
+            _id: '3',
+            name: 'Mikalla Conditioner',
+            category: 'Hair',
+            image: '/images/conditioner.png',
+            price: 6.99,
+            brand: 'Mikalla',
+            rating: 4,
+            numReviews: 13,
+            description: 'good quality conditioner'
+        },
+        {
+            _id: '4',
+            name: 'Unrefined Shea Butter',
+            category: 'Hair',
+            image: '/images/feat1.jpg',
+            price: 6.99,
+            brand: 'Unrefined',
+            rating: 4,
+            numReviews: 13,
+            description: 'good quality shea butter'
+        },
+        {
+            _id: '5',
+            name: 'African Black Soap',
+            category: 'Soap',
+            image: '/images/prod5.jpg',
+            price: 6.99,
+            brand: 'Unrefined',
+            rating: 4,
+            numReviews: 13,
+            description: 'good quality soap'
+        },
+        {
+            _id: '6',
+            name: 'Mikalla Conditioner',
+            category: 'Hair',
+            image: '/images/conditioner.png',
+            price: 6.99,
+            brand: 'Mikalla',
+            rating: 4,
+            numReviews: 13,
+            description: 'good quality conditioner'
+        },
+        {
+            _id: '7',
+            name: 'Unrefined Shea Butter',
+            category: 'Cosmetics',
+            image: '/images/feat1.jpg',
+            price: 3.99,
+            brand: 'organics',
+            rating: 4,
+            numReviews: 13,
+            description: 'good quality shampoo'
+        },
+        {
+            _id: '8',
+            name: 'Mikalla Conditioner',
+            category: 'Hair',
+            image: '/images/conditioner.png',
+            price: 7.99,
+            brand: 'Mikalla',
+            rating: 4,
+            numReviews: 13,
+            description: 'good quality conditioner'
+        },
+    ],
+};
+
+export default data;
