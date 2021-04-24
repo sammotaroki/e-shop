8dffe28d344b278d690ed705b6024daac7bafd08
samm <sammotaroki@gmail.com>
2021-04-24 11:08:00 +0300

    Orders api

---
+++ frontend/src/screens/OrderScreen.js
@@ -0,0 +1,129 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { detailsOrder } from '../actions/orderActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+
+export default function OrderScreen(props) {
+
+    const orderId = props.match.params.id;
+
+    const orderDetails = useSelector((state) => state.orderDetails);
+    const { order, loading, error } = orderDetails;
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(detailsOrder(orderId));
+    }, [dispatch, orderId]);
+
+    return loading ? (<LoadingBox></LoadingBox>)
+        : error ? (<MessageBox variant="danger">{error}</MessageBox>)
+            :
+            (
+                <div>
+                    <h1>Order No. {order._id}</h1>
+                    <div className="row inv">
+                        <div className="col-2">
+                            <ul>
+                                <li>
+                                    <div className="card card-body fade">
+                                        <h2>Shipping</h2>
+                                        <p>
+                                            <strong>Name : </strong>{order.shippingAddress.fullName} <br />
+                                            <strong>Address : </strong>
+                                            {order.shippingAddress.address}. {order.shippingAddress.city},  {order.shippingAddress.postalCode}. {order.shippingAddress.country}
+
+                                        </p>
+                                        {order.isDelivered ? (
+                                            <MessageBox variant="success">Delivered on {order.deliveredAt}</MessageBox>
+                                        ) : (
+                                            <MessageBox variant="danger">Awaiting delivery</MessageBox>
+                                        )}
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="card card-body fade">
+                                        <h2>Payment</h2>
+                                        <p>
+                                            <strong>Method :</strong> {order.paymentMethod}
+                                        </p>
+                                        {order.isPaid ? (
+                                            <MessageBox variant="success">Paid on {order.paidAt}</MessageBox>
+                                        ) : (
+                                            <MessageBox variant="danger">Awaiting payment</MessageBox>
+                                        )}
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="card card-body ordr">
+                                        <h2>Shopping Cart</h2>
+                                        <ul>
+                                            {order.orderItems.map((item) => (
+                                                <li key={item.product}>
+                                                    <div className="row">
+                                                        <div>
+                                                            <img
+                                                                src={item.image}
+                                                                alt={item.name}
+                                                                className="small"
+                                                            ></img>
+                                                        </div>
+                                                        <div className="min-30">
+                                                            <Link className="item-name cart" to={`/product/${item.product}`}>{item.name}</Link>
+                                                        </div>
+
+                                                        <div className="price cart">
+                                                            <span className="tiny">({item.qty} * {item.price})</span> {item.qty * item.price}
+                                                        </div>
+
+                                                    </div>
+                                                </li>
+                                            ))
+                                            }
+                                        </ul>
+
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-1">
+                            <div className="card card-body fade">
+                                <ul>
+                                    <li>
+                                        <h2>Order Summary</h2>
+                                    </li>
+                                    <li>
+                                        <div className="row prod">
+                                            <div>Items</div>
+                                            <div>€ {order.itemsPrice.toFixed(2)}</div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="row prod">
+                                            <div>Shipping</div>
+                                            <div>€ {order.shippingPrice.toFixed(2)}</div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="row prod">
+                                            <div>Tax</div>
+                                            <div>€ {order.taxPrice.toFixed(2)}</div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="row prod">
+                                            <div><strong>Order Total</strong></div>
+                                            <div><strong>€ {order.totalPrice.toFixed(2)}</strong></div>
+                                        </div>
+                                    </li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+}
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { createOrder } from '../actions/orderActions';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { ORDER_CREATE_RESET } from '../constants/orderConstants';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function PlaceOrderScreen(props) {
 
@@ -9,6 +13,9 @@ export default function PlaceOrderScreen(props) {
     if (!cart.paymentMethod) {
         props.history.push('/payment');
     }
+
+    const orderCreate = useSelector(state => state.orderCreate);
+    const { loading, success, error, order } = orderCreate;
 
     const toPrice = (num) => Number(num.toFixed(2));
 
@@ -18,9 +25,18 @@ export default function PlaceOrderScreen(props) {
     cart.taxPrice = toPrice(0.15 * cart.itemsPrice);
     cart.totalPrice = toPrice(cart.itemsPrice + cart.shippingPrice + cart.taxPrice);
 
-    const placeOrderHandler = () => {
+    const dispatch = useDispatch();
 
+    const placeOrderHandler = () => {
+        dispatch(createOrder({ ...cart, orderItems: cart.cartItems }));
     }
+
+    useEffect(() => {
+        if (success) {
+            props.history.push(`/order/${order._id}`);
+            dispatch({ type: ORDER_CREATE_RESET });
+        }
+    }, [dispatch, order, props.history, success]);
 
     return (
         <div>
@@ -119,6 +135,8 @@ export default function PlaceOrderScreen(props) {
                                     Place Order
                                 </button>
                             </li>
+                            {loading && <LoadingBox></LoadingBox>}
+                            {error && <MessageBox variant="danger">{error}</MessageBox>}
                         </ul>
                     </div>
                 </div>
